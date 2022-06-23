720d597b4fed882e8a867c8d67a54981375295ed
Ash Fallon <ashfallon23@gmail.com>
2022-06-23 13:46:25 +0100

    Added styling to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,7 +15,18 @@ const SearchBar = () => {
     setBestMatches(dummySymbolLookupData.result); // Dummy data for testing
   };
 
-  return <div>SearchBar</div>;
+  return (
+    <div className='flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200'>
+      <input
+        type='text'
+        value={input}
+        className='w-full px-4 py-2 focus:outline-none rounded-md'
+        placeholder='Search asset...'
+        onChange={e => setInput(e.target.value)}
+        onKeyDown={e => e.key === 'Enter' && bestMatchesHandler()}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
