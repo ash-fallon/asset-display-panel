a975f90353fd2af04bfe08a517ba0d7aad67149d
Ash Fallon <ashfallon23@gmail.com>
2022-06-25 14:23:13 +0100

    Added context store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
 import MainDisplay from './components/MainDisplay';
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return <MainDisplay />;
 };
 
