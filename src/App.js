136e6c0cb6b01b1625654e32ced5e57c13d39555
Ash Fallon <ashfallon23@gmail.com>
2022-06-25 15:15:16 +0100

    Added asset context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 
 import MainDisplay from './components/MainDisplay';
+import AssetContext from './context/asset-context';
 import ThemeContext from './context/theme-context';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [assetSymbol, setAssetSymbol] = useState('FB');
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <MainDisplay />
+      <AssetContext value={{ assetSymbol, setAssetSymbol }}>
+        <MainDisplay />
+      </AssetContext>
     </ThemeContext.Provider>
   );
 };
