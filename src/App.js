45842a6f99c73c4c209618cd7a72646c3186b1fa
Ash Fallon <ashfallon23@gmail.com>
2022-06-25 14:36:25 +0100

    Added context to DarkModeIcon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 
 import MainDisplay from './components/MainDisplay';
+import ThemeContext from './context/theme-context';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
-  return <MainDisplay />;
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <MainDisplay />
+    </ThemeContext.Provider>
+  );
 };
 
 export default App;
--- src/components/DarkModeIcon.js
+++ src/components/DarkModeIcon.js
@@ -1,9 +1,29 @@
+import { useContext } from 'react';
 import { MoonIcon } from '@heroicons/react/solid';
 
+import ThemeContext from './../context/theme-context';
+
 const DarkModeIcon = () => {
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <button className='rounded-lg border border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg'>
-      <MoonIcon className='h-8 w-8 cursor-pointer stroke-1 fill-transparent stroke-neutral-400' />
+    <button
+      className={`rounded-lg border border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg ${
+        darkMode ? 'shadow-gray-100' : null
+      }`}
+      onClick={toggleDarkMode}
+    >
+      <MoonIcon
+        className={`h-8 w-8 cursor-pointer stroke-1  ${
+          darkMode
+            ? 'fill-yellow-400 stroke-yellow-400'
+            : 'fill-transparent stroke-neutral-400'
+        }`}
+      />
     </button>
   );
 };
